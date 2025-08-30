422c9f69f3a507af74990db742a7854671107030
mishuk09 <mishukinfo09@gmail.com>
2025-08-30 17:32:49 +0800

    add if swith and loop code

---
+++ Core Fundamentals (must be crystal clear)/Control Flow (if, switch, loops)/if_switch_loops.js
@@ -0,0 +1,42 @@
+let a = 20;
+let b = 10;
+
+// if,else 
+if (a > b) {
+    console.log(a + " is greater than " + b);
+} else {
+    console.log(a + " is not greater than " + b);
+}
+
+// switch case
+
+let day = 3;
+
+switch (day) {
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    default:
+        console.log("Invalid day");
+}
+
+
+//loop
+
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+
+
+let j = 5;
+while (j < 20) {
+    console.log(j);
+    j++;
+}
---
+++ Core Fundamentals (must be crystal clear)/Operators & Expressions/Operators.js
@@ -0,0 +1,35 @@
+let a = 20;
+let b = 10;
+
+// Arithmetic operators
+function Arithmetic() {
+    console.log(a + b);
+    console.log(a - b);
+    console.log(a * b);
+    console.log(a / b);
+    console.log(a % b);
+}
+Arithmetic();
+
+
+// Comparison operators
+
+function Comparison() {
+    console.log(a == b);
+    console.log(a != b);
+    console.log(a > b);
+    console.log(a < b);
+    console.log(a >= b);
+    console.log(a <= b);
+}
+Comparison();
+
+
+// Logical operators
+
+function Logical() {
+    console.log(true && false);
+    console.log(true || false);
+    console.log(!a);
+}
+Logical();
